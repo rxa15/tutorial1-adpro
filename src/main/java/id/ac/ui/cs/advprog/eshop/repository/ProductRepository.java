69b51f330ef3113d5364b7f339948ba175f03d66
rxa15 <114054405+rxa15@users.noreply.github.com>
2024-02-06 21:15:57 +0700

    Created edit function to edit product

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -12,10 +12,23 @@ public class ProductRepository {
 
     public Product create(Product product){
         productData.add(product);
+        product.setProductId(String.valueOf(productData.size()));
         return product;
     }
 
+    public Product edit(Product product){
+        for(Product currentProduct : productData){
+            if(currentProduct.getProductId().equals(product.getProductId())){
+                currentProduct.setProductName(product.getProductName());
+                currentProduct.setProductQuantity(product.getProductQuantity());
+                return currentProduct;
+            }
+        }
+        return null; // product dijamin ditemukan di list product
+    }
+
     public Iterator<Product> findAll(){
+
         return productData.iterator();
     }
 }
