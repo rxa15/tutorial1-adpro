0c6c749e0eae6fbbcce7cf44c93eb0e93ddb4c9d
rxa15 <114054405+rxa15@users.noreply.github.com>
2024-02-14 18:25:41 +0700

    Fix case sensitive for HTML string return in Controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.*;
 public class HomePageController {
     @GetMapping("/")
     public String homePage(){
-        return "homepage";
+        return "HomePage";
     }
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -20,7 +20,7 @@ public class ProductController {
     public String createProductPage(Model model){
         Product product = new Product();
         model.addAttribute("product", product);
-        return "createProduct";
+        return "CreateProduct";
     }
 
     @PostMapping("/create")
@@ -33,14 +33,14 @@ public class ProductController {
     public String productListPage(Model model){
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
-        return "listProduct";
+        return "ListProduct";
     }
 
     @GetMapping("/edit/{id}")
     public String editProductPage(Model model, @PathVariable("id") String productId){
         Product product = service.findProductById(productId);
         model.addAttribute("product", product);
-        return "editProduct";
+        return "EditProduct";
     }
 
     @PostMapping("/edit/{id}")
