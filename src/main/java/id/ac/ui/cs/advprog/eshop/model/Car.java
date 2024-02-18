98a00e0fb84bfb17346176b6eb5d6730b92e80ec
rxa15 <114054405+rxa15@users.noreply.github.com>
2024-02-18 12:00:21 +0700

    Create a Car model in model package

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+public class Car {
+    public String carId;
+    public String carName;
+    public String carColor;
+    public int carQuantity;
+}
