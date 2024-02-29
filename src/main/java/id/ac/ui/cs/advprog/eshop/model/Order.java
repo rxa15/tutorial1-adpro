cf28ee2395eabd7d002245a7f8eeb1bab0136b79
rxa15 <114054405+rxa15@users.noreply.github.com>
2024-02-29 16:42:25 +0700

    [RED] Add tests for Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import java.util.Arrays;
 import java.util.List;
 
 import lombok.Builder;
@@ -17,8 +18,26 @@ public class Order {
     String status;
 
     public Order(String id, List<Product> products, Long orderTime, String author){
+        this.id = id;
+        this.orderTime = orderTime;
+        this.author = author;
+        this.status = "WAITING_PAYMENT";
+
+        if(products.isEmpty()){
+            throw new IllegalArgumentException();
+        } else{
+            this.products = products;
+        }
     }
 
     public Order(String id, List<Product> products, Long orderTime, String author, String status){
+        this(id, products, orderTime, author);
+
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+        if(Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))){
+            throw new IllegalArgumentException();
+        } else{
+            this.status = status;
+        }
     }
 }
